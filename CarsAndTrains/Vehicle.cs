08e68e62c8f6932c57077ea70213ed44d3118fa2
Czekoladov <74244258+Czekoladov@users.noreply.github.com>
2021-05-17 15:47:02 +0200

    Add Classes and firsts operations
    Kodowanie to moja pasja,
    całe życie wegetacja.

---
+++ CarsAndTrains/Vehicle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CarsAndTrains
+{
+    abstract public class Vehicle
+    {
+        public Boolean isColiding { get; set; }
+        public Boolean isVisible { get; set; }
+        public Boolean arrived { get; set; }
+        public int deathAfterArivalTime { get; set; } //add Rand()
+        private int vehicleSpeed { get; set; }
+        private int currentSpeed { get; set; }
+        private int []rememberedNodes = new int[2]; //dodaj get set
+        private List<int> nodeList= new List<int> { };//dodaj get set
+        private Image currentGraphics { get; set; }
+        private Image widthGraphics { get; set; }
+
+
+        public Vehicle() { 
+        
+        }
+
+        public Vehicle(Boolean isColding, Boolean isVisible, int deathAfterArivalTime, int vehicleSpeed, int currentSpeed, int[] rememberedNodes, 
+            List<int> nodeList, Image currentGraphics, Image widthGraphics) 
+        {
+            this.isColiding = isColding;
+            this.isVisible = isVisible;
+            this.deathAfterArivalTime = deathAfterArivalTime;
+            this.vehicleSpeed = vehicleSpeed;
+            this.currentSpeed = currentSpeed;
+            this.rememberedNodes = rememberedNodes;
+            this.nodeList = nodeList;
+            this.currentGraphics = currentGraphics;
+            this.widthGraphics = widthGraphics;
+        
+        }
+
+        virtual public void updateVehicle() 
+        {
+        
+        }
+
+        private void emptiedNodesAction()
+        {
+
+        }
+
+        protected void getNewGraphic()
+        { 
+        
+        }
+    }
+}
