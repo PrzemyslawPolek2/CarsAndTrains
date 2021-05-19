3af60d15d2320339d0d688905460f7bd7ad8e21b
Pydzik <74244783+Pydzik@users.noreply.github.com>
2021-05-19 20:46:18 +0200

    Creating files

---
+++ CarsAndTrains/TrainTriggerNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsAndTrains
+{
+    class TrainTriggerNode : Node
+    {
+        private Boolean isTriggered;
+        private Node rogatka;
+
+        public TrainTriggerNode()
+        {
+
+        }
+
+        public void triggerRogatka()
+        {
+
+        }
+        private void sendTrigger()
+        {
+
+        }
+    }
+}
