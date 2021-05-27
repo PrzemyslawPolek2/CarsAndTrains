489e184abbc289382709966f6febac2ca91a97e8
RolePoloGame <78389182+RolePoloGame@users.noreply.github.com>
2021-05-27 16:36:59 +0200

    Sketched

---
+++ CarsAndTrains/Enums/GraphicDirection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsAndTrains.Enums
+{
+    enum GraphicDirection
+    {
+        UP,
+        UP_LEFT,
+        LEFT,
+        DOWN_LEFT,
+        DOWN,
+        DOWN_RIGT,
+        RIGHT,
+        UP_RIGHT
+    }
+}
--- CarsAndTrains/MainWindow.xaml.cs
+++ CarsAndTrains/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CarsAndTrains.Classes;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,22 +29,74 @@ namespace CarsAndTrains
         {
             GetMain = this;
             InitializeComponent();
-            clickPositionL.Content = "[" + GetMain.getCanvasHeight() + ";" + GetMain.getCanvasWidth() + "]";
+            PublicAvaliableReferences.Initialize(canvas);
+            clickPositionL.Content = "[" + GetMain.GetCanvasHeight() + ";" + GetMain.GetCanvasWidth() + "]";
+            Thread thread =
+            new Thread(
+              _ => MoveFakeCar(exampleCar, new Point(1, 0))
+            );
+            thread.Start();
         }
 
         private void CanvasMouseDownEventHandler(object sender, MouseButtonEventArgs e)
         {
             Point p = Mouse.GetPosition(canvas);
-            clickPositionL.Content = "[" + p.X + ";" + p.Y + "]";
+            string str = p.X + " " + p.Y;
+            string path = System.Reflection.Assembly.GetEntryAssembly().Location;
+            path = System.IO.Path.GetDirectoryName(path) + "/nodePositions.txt";
+            clickPositionL.Content = str;
+
+            return;
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(str);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(str);
+                }
+            }
+
+            //
+            //using (StreamReader sr = File.OpenText(path))
+            //{
+            //    string s = "";
+            //    while ((s = sr.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //}
+            
         }
 
-        public double getCanvasWidth()
+        public double GetCanvasWidth()
         {
             return canvas.Width;
         }
-        public double getCanvasHeight()
+        public double GetCanvasHeight()
         {
             return canvas.Height;
+        }
+
+        private void MoveFakeCar(UIElement movedObject, Point movementVector)
+        {
+            double xMovement = movementVector.X;
+            double yMovement = movementVector.Y;
+            double speed = 1.0f;
+            for (int i = 0; i < 10000; i++)
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    Canvas.SetLeft(movedObject, Canvas.GetLeft(movedObject) + speed * xMovement);
+                    Canvas.SetTop(movedObject, Canvas.GetTop(movedObject) + speed * yMovement);
+                });
+                Thread.Sleep(10);
+            }
         }
     }
 }
